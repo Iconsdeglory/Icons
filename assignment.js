82f68e6255d521e4c7e3e09b2068ac570bf8acef
Iconsdeglory <123548571+Iconsdeglory@users.noreply.github.com>
2024-11-06 12:22:20 +0000

    Add files via upload

---
+++ assignment.js
@@ -0,0 +1,8 @@
+function calculateArea(width, height) {
+    const area = width * height;
+    return area;
+}
+const sampleWidth = 10;
+const sampleHeight = 6;
+const result = calculateArea(sampleWidth, sampleHeight);
+console.log("The area of the reactangle is " + result);
